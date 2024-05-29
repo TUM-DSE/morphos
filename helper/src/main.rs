af990e931bd64e7c99a243c1ae9d847851285a4e
paulzhng <paul.zhang@tum.de>
2024-05-29 21:11:13 +0000

    Improve reconfiguration helper

--- helper/src/main.rs
+++ helper/src/main.rs
@@ -5,11 +5,8 @@ use anyhow::{bail, Context};
 fn main() -> anyhow::Result<()> {
     let arg = args().nth(1);
     match arg.as_deref() {
-        Some("pass") => {
-            pass()?;
-        }
-        Some("drop") => {
-            drop()?;
+        Some("reconfigure") => {
+            reconfigure()?;
         }
         Some("send-packet") => {
             send_packet()?;
@@ -23,28 +20,14 @@ fn main() -> anyhow::Result<()> {
 const CONTROL_ADDR: &str = "173.44.0.2:4444";
 const DATA_ADDR: &str = "172.44.0.2:4444";
 
-fn drop() -> anyhow::Result<()> {
-    let new_program = b"drop";
+fn reconfigure() -> anyhow::Result<()> {
+    let new_program = args().nth(2).context("new program needs to be passed")?;
 
     let mut data = Vec::new();
     data.extend_from_slice(b"control");
     data.extend_from_slice(&1u64.to_le_bytes());
     data.extend_from_slice(&(new_program.len() as u64).to_le_bytes());
-    data.extend_from_slice(new_program);
-
-    socket()?.send_to(&data, CONTROL_ADDR).context("couldn't send packet")?;
-
-    Ok(())
-}
-
-fn pass() -> anyhow::Result<()> {
-    let new_program = b"pass";
-
-    let mut data = Vec::new();
-    data.extend_from_slice(b"control");
-    data.extend_from_slice(&1u64.to_le_bytes());
-    data.extend_from_slice(&(new_program.len() as u64).to_le_bytes());
-    data.extend_from_slice(new_program);
+    data.extend_from_slice(new_program.as_bytes());
 
     socket()?.send_to(&data, CONTROL_ADDR).context("couldn't send packet")?;
 
