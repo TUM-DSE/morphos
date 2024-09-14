265b782e494804aca17a4dd9fc1a8933c3e826a4
paulzhng <paul.zhang@tum.de>
2024-09-14 18:17:59 +0000

    Refine state-migration example

--- ebpf/src/bin/state-migration-v2.rs
+++ ebpf/src/bin/state-migration-v2.rs
@@ -5,14 +5,8 @@ use aya_ebpf::macros::map;
 use aya_ebpf::maps::Array;
 use bpf_element::filter::FilterResult;
 
-#[map(name = "PACKET_CTR_V1")]
-static PACKET_CTR_V1: Array<u32> = Array::with_max_entries(1, 0);
-
 #[map(name = "PACKET_CTR_V2")]
 static PACKET_CTR_V2: Array<u64> = Array::with_max_entries(1, 0);
-
-#[map(name = "VERSION")]
-static VERSION: Array<u64> = Array::with_max_entries(1, 0);
 
 #[no_mangle]
 #[link_section = "bpffilter"]
@@ -35,22 +29,23 @@ pub extern "C" fn main() -> FilterResult {
     }
 }
 
+#[map(name = "PACKET_CTR_V1")]
+static PACKET_CTR_V1: Array<u32> = Array::with_max_entries(1, 0);
+
+#[map(name = "VERSION")]
+static VERSION: Array<u64> = Array::with_max_entries(1, 0);
+
 fn handle_migration() -> Result<(), ()> {
-    let Some(current_version) = VERSION.get_ptr_mut(0) else {
-        return Err(());
-    };
+    let current_version = VERSION.get_ptr_mut(0).ok_or(())?;
 
     if unsafe { *current_version } != 2 {
-        let Some(old_counter) = PACKET_CTR_V1.get(0) else {
-            return Err(())
-        };
+        let old_counter = PACKET_CTR_V1.get(0).ok_or(())?;
+        let new_counter = PACKET_CTR_V2.get_ptr_mut(0).ok_or(())?;
 
-        let Some(new_counter) = PACKET_CTR_V2.get_ptr_mut(0) else {
-            return Err(())
+        unsafe {
+            *new_counter = *old_counter as u64;
+            *current_version = 2;
         };
-
-        unsafe { *new_counter = *old_counter as u64 };
-        unsafe { *current_version = 2 };
     }
 
     Ok(())
