873b9e55c6703ff3b4e56e095cef8f4555b7ee46
Pogobanane <aenderboy@gmx.de>
2025-01-10 12:49:22 +0000

    StringMatcher: match variable length words

--- ebpf/src/bin/stringmatcher.rs
+++ ebpf/src/bin/stringmatcher.rs
@@ -15,33 +15,42 @@ pub extern "C" fn main(ctx: *mut BpfContext) -> FilterResult {
     try_filter(&mut ctx).unwrap_or_else(|_| FilterResult::Abort)
 }
 
+/***
+* A very simple string matching filter that drops packets that contains the bytes 'leetcodew
+*/
 #[inline(always)]
 fn try_filter(ctx: &mut BpfContext) -> Result<FilterResult, ()> {
     let ipv4hdr: *const Ipv4Hdr = unsafe { ctx.get_ptr(0)? };
 
+    let match_word: [u8; 8] = [
+        'l' as u8,
+        'e' as u8,
+        'e' as u8,
+        't' as u8,
+        'c' as u8,
+        'o' as u8,
+        'd' as u8,
+        'e' as u8
+    ];
     let b0: u8 = 0x22;
     let b1: u8 = 0x8d;
     let mut state: u16 = 0;
 
-    for i in 0..2000 { // we consider packets of size 2k at max
+    for i in 0..2000 {
+        // we consider packets of size 2k at max
         let inspectable: *const u8 = match unsafe { ctx.get_ptr(i) } {
             Ok(a) => a,
             Err(_) => break, // end of packet. Stop loop
         };
         let writable: &mut u8 = unsafe { &mut *ctx.get_ptr_mut(0)? };
         let inspectable: u8 = unsafe { *inspectable };
-        match state {
-            0 if inspectable == b0 => { state += 1; },
-            0 if inspectable != b0 => { state = 0;  },
-            1 if inspectable == b1 => { state += 1; },
-            1 if inspectable != b1 => { state = 0;  },
-            2 => {
-                return Ok(FilterResult::Drop);
-                // *writable = i as u8;
-                // return Ok(FilterResult::Pass);
-            }
-            _ => { return Err(()) }, // programmer forgot to handle a state
-            // _ => { *writable = i as u8; break },
+        if state >= 8 {
+            return Ok(FilterResult::Drop); // we tested and found all states (match_word bytes)
+        }
+        if inspectable == match_word[state as usize] {
+            state += 1;
+        } else {
+            state = 0;
         }
     }
 
