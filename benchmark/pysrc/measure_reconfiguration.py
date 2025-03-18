6c2cef0bdfa2af4f83379f5828096c10fd875e65
Pogobanane <aenderboy@gmx.de>
2025-03-18 15:36:12 +0000

    measure_reconfiguration: automate xdp

--- benchmark/pysrc/measure_reconfiguration.py
+++ benchmark/pysrc/measure_reconfiguration.py
@@ -25,126 +25,42 @@ import click_configs
 from conf import G
 
 unikraft_interface = "0"
-safe_vpp_warmup = False # without we rarely get excessive standard deviations
-GUEST_IP = "10.10.0.1"
-TEST_CLIENT_IP = "10.10.0.2"
-
-def click_tx_config(interface: str, size: int = 1460, dst_mac: str = "90:e2:ba:c3:76:6e", extra_processing: str = "") -> str:
-    # size - 4 bytes ethernet CRC - 14 ethernet header - 20 IP header - 20 ethernet peamble maybe?
-    size = size - 4 - 14 - 20 - 20
-    return f"""
-//Default values for packet length, number of packets and amountfs of time to replay them
-define($L 60, $R 0, $S 100000);
-
-//You do not need to change these to the real ones, just have the dmac match the receiver's one
-define($mymac 90:e2:ba:c3:79:66)
-define($dmac {dst_mac})
-//Ip are just for a convenient payload as this is l2
-define($myip 192.168.130.13)
-define($dstip 192.168.128.13)
-
-//Explained in loop.click
-define($verbose 3)
-define($blocking true)
-
-
-InfiniteSource(DATA \<0800>, LENGTH {size}, LIMIT -1, BURST 100000)
-// InfiniteSource2(DATA \<0800>, SRCIP $myip, DSTIP $dstip, LENGTH {size}, LIMIT -1, BURST 100000)
- -> UDPIPEncap($myip, 5678, $dstip, 5678)
-{extra_processing}
--> EtherEncap(0x0800, $mymac, $dmac)
-// FastUDPFlows(RATE 0, LIMIT -1, LENGTH {size}, SRCETH $mymac, DSTETH $dmac, SRCIP $myip, DSTIP $dstip, FLOWS 1, FLOWSIZE 100000)
--> ic0 :: AverageCounter()
--> ToDevice({interface});
-
-fd :: FromDevice({interface}) -> Print('rx') -> Discard
-
-Script(TYPE ACTIVE,
-       wait 5ms,
-       label start,
-       print "Number of packets sent : $(ic0.rate)",
-       wait 1s,
-       goto start
-       )
-"""
-
-def click_rx_config(interface: str, extra_processing: str = "") -> str:
-    return f"""
-from :: FromDevice({interface})
-{extra_processing}
--> ic0 :: AverageCounter()
--> Discard;
-
-Script(TYPE ACTIVE,
-       wait 5ms,
-       label start,
-       print "Rx rate: $(ic0.rate)",
-       write ic0.reset 1,
-       write from.reset 1,
-       wait 1s,
-       goto start
-       )
-    """
-
-bmon_format = "format:fmt=\$(attr:rxrate:packets)\t\$(attr:rxrate:bytes)\n"
 
 @dataclass
-class ThroughputTest(AbstractBenchTest):
+class ReconfigurationTest(AbstractBenchTest):
 
-    # test options
-    direction: str # VM's point of view: rx | tx
-
-    interface: str # network interface used
-    size: int # packet size
     vnf: str # workload
-    system: str # linux | uk | ukebpf | ukebpfjit
+    system: str # linux | uk | ukebpfjit
 
     def test_infix(self):
-        return f"throughput_{self.system}_{self.interface}_{self.direction}_{self.vnf}_{self.size}B"
-
-    def pktgen_output_filepath(self, repetition: int) -> str:
-        return self.output_filepath(repetition, extension="pktgen.log")
+        return f"reconfiguration_{self.system}_{self.vnf}"
 
     def estimated_runtime(self) -> float:
         """
         estimate time needed to run this benchmark excluding boot time in seconds
         """
-        overheads = 5
-        return (self.repetitions * (G.DURATION_S + 2) ) + overheads
+        return 0
 
 
     def parse_results(self, repetition: int) -> DataFrame:
-        warmup = 4 # how many seconds to skip at the beginning
-        if safe_vpp_warmup and Interface(self.interface).needs_vpp():
-            warmup = 10 # vpp implements active queue management which delays packet bursts
-
         values = []
-        if self.direction == "rx":
-            # parse click log
+        if self.system == "xdp":
+            # parse output
             with open(self.output_filepath(repetition), 'r') as f:
-                lines = f.readlines()
-            lines = [ line for line in lines if "Rx rate: " in line ]
-            # pps values
-            values = [ float(line.split("Rx rate: ")[1].strip().split(" ")[0].strip()) for line in lines ]
-            values = values[warmup:]
-
-        elif self.direction == "tx":
-            # parse click log
-            with open(self.output_filepath(repetition), 'r') as f:
-                lines = f.readlines()
-            # pps values
-            values = [ float(line.split("\t")[0].strip()) for line in lines ]
-            values = values[warmup:]
-
+                for line in f.readlines():
+                    if line.startswith("real"):
+                        time_str = line.split("\t")[1].strip()
+                        time_s = float(time_str.split("m")[0]) * 60 + float(time_str.split("m")[1][:-1])
+                        values += [ int(time_s*1000000000) ]
         else:
-            raise ValueError(f"Unknown direction: {self.direction}")
+            raise ValueError(f"Unknown system: {self.system}")
 
         data = []
         for value in values:
             data += [{
                 **asdict(self), # put selfs member variables and values into this dict
                 "repetition": repetition,
-                "pps": value,
+                "nsec": value,
             }]
         return DataFrame(data=data)
 
@@ -411,60 +327,37 @@ def main(measurement: Measurement, plan_only: bool = False) -> None:
     host, loadgen = measurement.hosts()
 
     # set up test plan
-    interfaces = [
-          Interface.VPP,
-          # Interface.BRIDGE_VHOST,
-          ]
-    directions = [ "rx", "tx" ]
     systems = [ "linux", "uk", "ukebpfjit" ]
     vm_nums = [ 1 ]
-    sizes = [ 64, 256, 1024, 1518 ]
     vnfs = [ "empty", "filter", "nat", "ids", "mirror" ]
     repetitions = 3
     G.DURATION_S = 71 if not G.BRIEF else 15
-    if safe_vpp_warmup:
-        G.DURATION_S = max(30, G.DURATION_S)
     if G.BRIEF:
-        # interfaces = [ Interface.BRIDGE ]
-        # interfaces = [ Interface.BRIDGE_VHOST ]
-        interfaces = [ Interface.VPP ]
-        # interfaces = [ Interface.BRIDGE_VHOST, Interface.VPP ]
-        # directions = [ "rx", "tx" ]
-        directions = [ "tx" ]
         # systems = [ "linux", "uk", "ukebpfjit" ]
         # systems = [ "uk", "ukebpfjit" ]
         # systems = [ "uk" ]
-        systems = [ "ukebpfjit" ]
+        systems = [ "xdp" ]
         # systems = [ "linux" ]
-        vm_nums = [ 1 ]
-        # vm_nums = [ 128, 160 ]
         # vnfs = [ "empty" ]
-        sizes = [ 64 ]
         vnfs = [ "empty" ]
-        repetitions = 1
+        repetitions = 2
 
     def exclude(test):
-        return ((Interface(test.interface).is_passthrough() and test.num_vms > 1) or
-                    (test.vnf == "nat" and test.direction == "tx") or # packets get stuck in queue
-                    (test.vnf == "mirror" and test.direction == "tx") # bidirection test
-        )
+        return False
 
     test_matrix = dict(
         repetitions=[ repetitions ],
-        direction=directions,
-        interface=[ interface.value for interface in interfaces],
         num_vms=vm_nums,
-        size=sizes,
         vnf=vnfs,
         system=systems,
     )
-    tests: List[ThroughputTest] = []
-    tests = ThroughputTest.list_tests(test_matrix, exclude_test=exclude)
+    tests: List[ReconfigurationTest] = []
+    tests = ReconfigurationTest.list_tests(test_matrix, exclude_test=exclude)
 
 
-    args_reboot = ["interface", "num_vms", "direction", "system", "vnf", "size"]
+    args_reboot = ["num_vms", "system", "vnf"]
     info(f"ThroughputTest execution plan:")
-    ThroughputTest.estimate_time2(tests, args_reboot)
+    ReconfigurationTest.estimate_time2(tests, args_reboot)
 
     if plan_only:
         return
@@ -474,88 +367,37 @@ def main(measurement: Measurement, plan_only: bool = False) -> None:
             args_reboot = args_reboot,
             brief = G.BRIEF
             ) as (bench, bench_tests):
-        for [num_vms, interface, direction, system, vnf, size], a_tests in bench.multi_iterator(bench_tests, ["num_vms", "interface", "direction", "system", "vnf", "size"]):
-            interface = Interface(interface)
+        for [system, vnf], a_tests in bench.multi_iterator(bench_tests, ["system", "vnf"]):
 
-            info("Booting VM for this test matrix:")
-            info(ThroughputTest.test_matrix_string(a_tests))
+            # info("Booting VM for this test matrix:")
+            # info(ReconfigurationTest.test_matrix_string(a_tests))
 
             assert len(a_tests) == 1 # we have looped through all variables now, right?
             test = a_tests[0]
             info(f"Running {test}")
 
 
-            debug('Binding loadgen interface')
-            loadgen.modprobe_test_iface_drivers()
-            loadgen.release_test_iface() # bind linux driver
-            try:
-                loadgen.delete_nic_ip_addresses(loadgen.test_iface)
-            except Exception:
-                pass
-            if test.vnf == "nat":
-                loadgen.exec(f"sudo ip address add {TEST_CLIENT_IP}/32 dev {loadgen.test_iface}")
-            loadgen.setup_test_iface_ip_net()
-
-
-            if system in [ "uk", "ukebpf", "ukebpfjit" ]:
-                files, element = test.click_config()
-                click_config = ""
-                if test.vnf == "nat":
-                    click_config = click_configs.nat(
-                        interface=unikraft_interface,
-                        guest_ip=GUEST_IP,
-                        guest_mac=measurement.guest.test_iface_mac,
-                        gw_ip=strip_subnet_mask(loadgen.test_iface_ip_net),
-                        gw_mac=loadgen.test_iface_mac,
-                        src_ip=TEST_CLIENT_IP,
-                        src_mac=loadgen.test_iface_mac,
-                        dst_ip=strip_subnet_mask(loadgen.test_iface_ip_net),
-                        dst_mac=measurement.guest.test_iface_mac,
-                        size=test.size,
-                        direction=test.direction,
-                        rewriter=element
-                    )
-                elif test.vnf == "mirror":
-                    click_config = click_configs.mirror(
-                        interface=unikraft_interface,
-                        ip=strip_subnet_mask(loadgen.test_iface_ip_net),
-                        mac=loadgen.test_iface_mac,
-                        extra_element=element
-                    )
-                elif test.direction == "tx":
-                    click_config = click_tx_config(unikraft_interface, size=test.size, dst_mac=loadgen.test_iface_mac, extra_processing=element)
-                elif test.direction == "rx":
-                    click_config = click_rx_config(unikraft_interface, extra_processing=element)
-
-                remote_unikraft_log_raw  = "/tmp/unikraft.log" # will be cleared sometimes
-                remote_unikraft_init_log  = f"{remote_unikraft_log_raw}.init" # contains the startup log
-                host.exec(f"sudo rm {remote_unikraft_log_raw} || true")
-                host.exec(f"sudo rm {remote_unikraft_init_log} || true")
-
-                for repetition in range(repetitions): # restarting click for each repetition means restarting unikraft
-                    with measurement.unikraft_vm(interface, click_config, vm_log=remote_unikraft_log_raw, cpio_files=files) as guest:
-                        host.exec(f"sudo cp {remote_unikraft_log_raw} {remote_unikraft_init_log}")
-
-                        if test.direction == "tx":
-                            test.run_unikraft_tx(repetition, guest, loadgen, host, remote_unikraft_log_raw)
-                        elif test.direction == "rx":
-                            test.run_unikraft_rx(repetition, guest, loadgen, host, remote_unikraft_log_raw)
-                    # end VM
-
-            elif system == "linux":
-                # boot VMs
-                with measurement.virtual_machine(interface) as guest:
-                    for repetition in range(repetitions):
-
-                        if test.direction == "tx":
-                            test.run_linux_tx(repetition, guest, loadgen, host)
-                        elif test.direction == "rx":
-                            test.run_linux_rx(repetition, guest, loadgen, host)
-                    pass
-                # end VM
-
-            else:
-                raise ValueError(f"Unknown system: {system}")
+            if test.system == "uk":
+                print("")
+            elif test.system == "ukebpfjit":
+                print("")
+            elif test.system == "linux":
+                print("")
+            elif test.system == "xdp":
+                for repetition in range(repetitions):
+                    iface = "eno1"
+                    xdp_program = f"{host.project_root}/nix/builds/xdp/lib/reflector.o"
+                    remote_outfile = "/tmp/xdp.log"
+                    local_outfile = test.output_filepath(repetition)
+                    iterations = 30
+                    host.exec(f"sudo rm {remote_outfile} || true")
+                    # time must be executed in sh. Other shells use other time impls
+                    xdp_add_cmd = f"ip link set {iface} xdpgeneric obj {xdp_program} sec xdp"
+                    xdp_del_cmd = f"sudo ip link set {iface} xdpgeneric off || true"
+                    cmd = f"sudo /bin/sh -c \"for i in {{1..{iterations}}}; do {xdp_del_cmd}; time {xdp_add_cmd}; done\" 2>&1 | tee -a {remote_outfile}"
+                    host.exec(cmd)
+                    host.copy_from(remote_outfile, local_outfile)
+            breakpoint()
 
 
             bench.done(test)
@@ -578,10 +420,10 @@ def main(measurement: Measurement, plan_only: bool = False) -> None:
     # summarize results
     all_data = pd.concat(all_dfs)
     del all_data["repetition"]
-    all_data['mpps'] = all_data['pps'].apply(lambda pps: pps / 1_000_000)
-    del all_data["pps"]
-    df = all_data.groupby([ col for col in all_data.columns if col != "mpps" ]).describe()
-    with open(path_join(G.OUT_DIR, f"throughput_summary.log"), 'w') as file:
+    # all_data['mpps'] = all_data['pps'].apply(lambda pps: pps / 1_000_000)
+    # del all_data["pps"]
+    df = all_data.groupby([ col for col in all_data.columns if col != "nsec" ]).describe()
+    with open(path_join(G.OUT_DIR, "reconfiguration_summary.log"), 'w') as file:
         file.write(df.to_string())
 
 
